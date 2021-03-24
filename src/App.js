e88744df6405fd7fec824f40914c3180abb19d84
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2021-03-24 01:13:57 -0400

    completed the task manager

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Task from './Task'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+library.add(faTrash);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //define the state
+    this.state = {
+      items: [],
+      //object for the current item, stores the text and key
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    //hard bind the this value to the constructor function so the handleInput method doesn't lose it's context,,, do the same for addItem
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+
+  handleInput(e) {
+    //to change the state variable, use the method called setState
+    this.setState({
+      //inside this object are the variables that I want to update
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e) {
+    //prevent the button's default behavior (refreshing the page)
+    e.preventDefault();
+    //get the current item and store it in a variable called newItem
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    //check if the newItem.text is not empty
+    if (newItem.text !== "") {
+      //unpack all of the items in the list and converts them into individual items
+      const newItems = [...this.state.items, newItem];
+      //update the state variable
+      this.setState({
+        //set the items to the newItems array
+        items: newItems,
+        //now set the current item back to empty values 
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+  deleteItem(key) {
+    //property called filter that filters the items that meets the condition we give
+    //will filter all items that key doesn't match and store it in filteredItems
+    const filteredItems = this.state.items.filter(item =>
+      item.key !== key);
+    //update state
+    this.setState({
+    items: filteredItems
+  })
+}
+//define the function, it recieves the value and key
+setUpdate(text, key){
+  const items = this.state.items;
+  //loop through each item using the map function
+  items.map(item =>{
+    //check if the item key is equal to the key that is provided in the function
+    if(item.key===key){
+      //then change the text to the text value
+      item.text=text;
+    }
+  })
+  //update this state with the new items
+  this.setState({
+    items: items
+  })
+}
+
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Task"
+            //link the input field and current item by setting the value of the input field to currentItem.text
+            //variables and methods must be enclosed inside of curly braces because it is part of jsx when including javascript variable or method inside of an html file
+            value={this.state.currentItem.text}
+            //sets the onChange to the handleInput method
+            onChange={this.handleInput} />
+          <button type="submit">Add</button>
+        </form>
       </header>
+      <Task items={this.state.items}
+        deleteItem={this.deleteItem}
+        setUpdate ={this.setUpdate}></Task>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Task.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Task.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
+
+function Task(props) {
+    //get props.items and store in a new var called items so it is easier to access 
+    const items = props.items;
+    const tasks = items.map(item => {
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text"
+                    id={item.key}
+                    value={item.text}
+                    //onChange attribute needs to be defined 
+                    onChange={
+                        (e) => {
+                            //passing the value and key value as the parameters
+                            props.setUpdate(e.target.value, item.key)
+                        }
+                    }
+                />
+                <span>
+                    <FontAwesomeIcon className="faicons"
+                        icon='trash'
+                        //give the onClick property the method of props.deleteItem use arrow function to pass the referrence 
+                        //pass a key value to identify which item is being deleted
+                        onClick={() => props.deleteItem(item.key)
+                        } />
+                </span>
+            </p>
+        </div>
+    })
+    return (
+        <div>
+            <FlipMove duration={300} easing="ease-in-out">
+                {tasks}
+            </FlipMove>
+        </div>
+    )
+}
+
+
+
+
+export default Task;
